495869f13e0735a799fa3f2075eb7a56cc69a2cb
Sima3443 <toppochka@gmail.com>
2024-05-02 17:36:03 +0300

    save

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+def index(request) -> HttpResponse:
+    # products = Products.objects.first()
+    context = {
+        "title": 'Salamis',
+        # "products": products,
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request) -> HttpResponse:
+    context = {
+        "title": 'About us',
+    }
+    return render(request, 'main/about.html', context)

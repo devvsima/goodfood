9f1f3c3ea65fa1f0f848be1da020a68399b3df8e
devSima <devvsima@gmail.com>
2024-05-03 14:02:19 +0300

    save

--- catalog/views.py
+++ catalog/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def catalog(request):
-    print('hi')
-    return render(request, 'catalog/catalog.html')
+    return render(request, 'catalog/catalog.html', {'title': 'Home'})
